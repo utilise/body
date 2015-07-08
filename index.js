9e5876856b2a8b835f9618ca743062ed614636af
pemrouz <p3pedram@hotmail.com>
2015-07-08 03:24:16 +0100

    update body to match key syntax

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var key = require('key')
 
 module.exports = function body(ripple){
   return function(name){
-    return key([name, 'body'])(ripple.resources)
+    return key([name, 'body'].join('.'))(ripple.resources)
   }
 }
